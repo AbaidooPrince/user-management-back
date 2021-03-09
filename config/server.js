0011307494efa7bd7b546be8f1bc8a461142cfd8
PrinceAbaidoo <70167148+PrinceAbaidoo@users.noreply.github.com>
2021-03-09 16:43:24 +0000

    first push of backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5df2eaf1237828334843a24a610d6f09'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb74d5dc-ba1d-4313-918c-83f29bc71f36'
+};
